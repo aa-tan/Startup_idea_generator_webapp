0a6616080a76698c66ea5698b1f200ed1293d28a
aa-tan <a.tan@keio.jp>
2018-06-27 19:40:20 +0900

    Removed testing lines

--- src/server.cr
+++ src/server.cr
@@ -2,12 +2,7 @@ require "kemal"
 Kemal.config.port = 8090
 
 get "/" do |env|
-    render "views/home.ecr", "views/layouts/layout.ecr"
-end
-
-get "/name/:name" do |env|
-    name = env.params.url["name"]
-    render "views/hello.ecr"
+    render "views/main.ecr", "views/layouts/layout.ecr"
 end
 
 Kemal.run
